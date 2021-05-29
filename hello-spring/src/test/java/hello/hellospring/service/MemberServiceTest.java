80bd891fa9837bcd54acc8b430ae1dc33bc805d3
김지웅 <godjw96@gmail.com>
2021-05-29 20:38:37 +0900

    회원 서비스 개발

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,25 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void join() {
+        //given
+        
+        //when
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
