052f56043aaecdb0bbdd54ddfb6a5236426e3376
김지웅 <godjw96@gmail.com>
2021-06-28 15:27:24 +0900

    의존관계 주입

--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,8 +2,10 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
-public class FixDiscountPolicy implements DiscountPolicy{
+
+public class FixDiscountPolicy implements DiscountPolicy {
 
     private int disCountFixAmount = 1000; // 1000원 할인
 
---
+++ core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,31 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> polices;
+
+        @Autowired
+        DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> polices) {
+            this.policyMap = policyMap;
+            this.polices = polices;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + polices);
+        }
+    }
+}
