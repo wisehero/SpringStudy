5f4692246eb770cd69786dec8a8c24ad6fe6d62a
김지웅 <godjw96@gmail.com>
2021-07-24 14:01:31 +0900

    스프링 퀵스타트 챕터 1

---
+++ springquickstart/src/main/java/rubypaper/springquickstart/SpringquickstartApplication.java
@@ -0,0 +1,13 @@
+package rubypaper.springquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringquickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringquickstartApplication.class, args);
+	}
+
+}
---
+++ springquickstart/src/test/java/rubypaper/springquickstart/SpringquickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package rubypaper.springquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringquickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
