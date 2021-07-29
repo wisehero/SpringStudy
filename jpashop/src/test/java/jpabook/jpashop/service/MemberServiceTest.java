3d6afd1fd80f6bd2ab67f8d2259e68b15dcd3932
김지웅 <godjw96@gmail.com>
2021-07-29 16:37:30 +0900

    회원서비스 테스트 JUNIT5

--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -22,7 +22,7 @@ class MemberServiceTest {
     MemberRepository memberRepository;
 
     @Test
-     void 회원가입() throws Exception {
+    void 회원가입() throws Exception {
         //given
         Member member = new Member();
         member.setName("kim");
@@ -38,9 +38,18 @@ class MemberServiceTest {
     @Test
     void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
+
 
         //then
+        IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertEquals("이미 존재하는 회원입니다.", thrown.getMessage());
     }
 }
