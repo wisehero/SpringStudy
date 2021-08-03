a848d4a089997581204af20ab4524e126c74f138
김지웅 <godjw96@gmail.com>
2021-08-03 21:53:22 +0900

    게시판 만들기 셋팅

---
+++ boardDemo/src/test/java/board/boardDemo/BoardDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package board.boardDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
