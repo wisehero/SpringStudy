9c7d6d70ec79ec1cce62e27731cadee7662e6f9d
김지웅 <godjw96@gmail.com>
2021-08-12 16:52:54 +0900

    Update BoardController.java

--- boardDemo/src/main/java/board/boardDemo/controller/BoardController.java
+++ boardDemo/src/main/java/board/boardDemo/controller/BoardController.java
@@ -4,8 +4,7 @@ import board.boardDemo.dto.BoardDto;
 import board.boardDemo.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +32,34 @@ public class BoardController {
     @PostMapping("/post")
     public String write(BoardDto boardDto) {
         boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+
+    // 게시글 내용 확인
+    @GetMapping("/post/{id}")
+    public String detail(@PathVariable("id") long id, Model model) {
+        BoardDto boardDto = boardService.getPost(id);
+        model.addAttribute("post", boardDto);
+        return "board/detail.html";
+    }
+
+    // 게시글 수정 및 삭제
+    @GetMapping("/post/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        BoardDto boardDto = boardService.getPost(id);
+        model.addAttribute("post", boardDto);
+        return "board/edit.html";
+    }
+
+    @PutMapping("/post/edit/{id}")
+    public String update(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        boardService.deletePost(id);
         return "redirect:/";
     }
 }
