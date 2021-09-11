a075bab30efd4bf1a4c24c34724ca9b7659e7313
김지웅 <godjw96@gmail.com>
2021-09-11 20:06:30 +0900

    상품 레포지토리

---
+++ Project/springshop/src/main/java/com/example/springshop/repository/ItemRepository.java
@@ -0,0 +1,28 @@
+package com.example.springshop.repository;
+
+import com.example.springshop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemName(String itemName);
+
+    // Or 조건 처리 메서드
+    List<Item> findByItemNameOrItemDetail(String itemName, String itemDetail);
+
+    // LessThan
+    List<Item> findByPriceLessThan(Integer price);
+
+    //LessThan & OrderBy
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+
+
+}
