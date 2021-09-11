423299da1721767a0b74ff53b29bdf1826135a0e
김지웅 <godjw96@gmail.com>
2021-09-11 18:15:57 +0900

    프로젝트 세팅

---
+++ Project/springshop/src/main/java/com/example/springshop/SpringshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringshopApplication.class, args);
+	}
+
+}
