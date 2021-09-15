babd08341e3ee5b3553edaf6da05202e1cb2103b
김지웅 <godjw96@gmail.com>
2021-09-16 00:36:35 +0900

    회원 컨트롤러

---
+++ Project/springshop/src/main/java/com/example/springshop/controller/MemberController.java
@@ -0,0 +1,36 @@
+package com.example.springshop.controller;
+
+import com.example.springshop.dto.MemberFormDto;
+import com.example.springshop.entity.Member;
+import com.example.springshop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/new")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
+    @PostMapping("/new")
+    public String memberForm(MemberFormDto memberFormDto) {
+
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+
+        return "redirect:/";
+    }
+}
