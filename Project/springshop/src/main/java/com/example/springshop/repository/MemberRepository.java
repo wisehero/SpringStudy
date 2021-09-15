11a8ed97c6c44679e4bbc8343c114460633674aa
김지웅 <godjw96@gmail.com>
2021-09-15 23:21:20 +0900

    Create MemberRepository.java

---
+++ Project/springshop/src/main/java/com/example/springshop/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.springshop.repository;
+
+import com.example.springshop.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmail(String email);
+}
