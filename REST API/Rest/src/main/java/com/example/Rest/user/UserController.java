2bb16a2a9ab6267f337c68d24166455c24057e93
김지웅 <godjw96@gmail.com>
2021-09-16 23:07:17 +0900

    Create UserController.java

---
+++ REST API/Rest/src/main/java/com/example/Rest/user/UserController.java
@@ -0,0 +1,45 @@
+package com.example.Rest.user;
+
+
+import com.example.Rest.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUser() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOnd(id);
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not fount", id));
+        }
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity createUser(@RequestBody User user) {
+        User saved = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(saved.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
