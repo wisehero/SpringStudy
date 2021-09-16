cc907afca309ca9497f7d287ca281b514fd32043
김지웅 <godjw96@gmail.com>
2021-09-16 15:35:04 +0900

    Update SecurityConfig.java

--- Project/springshop/src/main/java/com/example/springshop/config/SecurityConfig.java
+++ Project/springshop/src/main/java/com/example/springshop/config/SecurityConfig.java
@@ -1,19 +1,40 @@
 package com.example.springshop.config;
 
+import com.example.springshop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MemberService memberService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage("/members/login").defaultSuccessUrl("/").usernameParameter("email")
+                .failureUrl("/members/login/error").and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/");
 
+        http.authorizeRequests().mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated();
+
+        http.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
     @Bean
@@ -21,4 +42,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+
+    /**
+     * 스프링 시큐리티에서 인증은 AuthenticationManager를 통해 이루어지며 빌더가 매니저를 생성한다.
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+    }
 }
