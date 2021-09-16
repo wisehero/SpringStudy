66cd3bc1951dbdc5488a442d00b1bf96c91d9007
김지웅 <godjw96@gmail.com>
2021-09-16 23:06:39 +0900

    REST API

--- REST API/Rest/src/main/java/com/example/Rest/helloworld/HelloController.java
+++ REST API/Rest/src/main/java/com/example/Rest/helloworld/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.Rest;
+package com.example.Rest.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
