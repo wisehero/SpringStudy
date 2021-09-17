507aa90ed2c35bf3e68a3373c3e5ca719a29068b
김지웅 <godjw96@gmail.com>
2021-09-17 21:45:18 +0900

    Create BaseTimeEntity.java

---
+++ Project/springshop/src/main/java/com/example/springshop/entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.example.springshop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@EntityListeners(value = {AuditingEntityListener.class}) // Auditing을 적용하기 위한 어노테이션
+@MappedSuperclass // 공통 매핑 정보가 필요할 때 사용하는 어노테이션
+@Getter
+@Setter
+public abstract class BaseTimeEntity {
+
+
+    @CreatedBy
+    @Column(updatable = false)
+    private LocalDateTime regTime;
+
+    @LastModifiedBy
+    private LocalDateTime updateTime;
+}
