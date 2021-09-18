ca9f869a497e9e8a1c8b1b4d8bf40ddab14d7df5
김지웅 <godjw96@gmail.com>
2021-09-18 16:30:44 +0900

    상품주문, 주문검색 및 취소

--- SpringJPA/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ SpringJPA/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -5,9 +5,12 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -50,5 +53,9 @@ public class OrderService {
         Order order = orderRepository.findOne(orderId);
         // 주문 취소
         order.cancel();
+    }
+
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByCriteria(orderSearch);
     }
 }
