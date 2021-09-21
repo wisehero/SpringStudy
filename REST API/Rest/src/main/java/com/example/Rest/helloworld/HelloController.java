3f66b43a145160762a39170389efadf2e4927486
김지웅 <godjw96@gmail.com>
2021-09-21 23:19:07 +0900

    메세지 국제화

--- REST API/Rest/src/main/java/com/example/Rest/helloworld/HelloController.java
+++ REST API/Rest/src/main/java/com/example/Rest/helloworld/HelloController.java
@@ -1,10 +1,18 @@
 package com.example.Rest.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -14,6 +22,11 @@ public class HelloController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 
 }
