c674c12ddc56ac3e0cea4ec2c69b4960a116a95b
김지웅 <godjw96@gmail.com>
2021-09-21 22:38:58 +0900

    ValidationAPI 사용

--- REST API/Rest/src/main/java/com/example/Rest/user/User.java
+++ REST API/Rest/src/main/java/com/example/Rest/user/User.java
@@ -3,6 +3,8 @@ package com.example.Rest.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -12,6 +14,9 @@ import java.util.Date;
 public class User {
 
     private Integer id;
+
+    @Size(min=2, message = "Name은 2글자 이상 입력해주세요. ")
     private String name;
+    @Past
     private Date joinDate;
 }
--- REST API/Rest/src/main/java/com/example/Rest/user/UserController.java
+++ REST API/Rest/src/main/java/com/example/Rest/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity createUser(@RequestBody User user) {
+    public ResponseEntity createUser(@Valid @RequestBody User user) {
         User saved = service.save(user);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
