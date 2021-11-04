ca31fe59af7381b4e919bb21cb1171afd0b4d067
김지웅 <godjw96@gmail.com>
2021-11-05 00:50:52 +0900

    rest setting

---
+++ resttutorial/src/main/java/com/example/resttutorial/ResttutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.resttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttutorialApplication.class, args);
+	}
+
+}
