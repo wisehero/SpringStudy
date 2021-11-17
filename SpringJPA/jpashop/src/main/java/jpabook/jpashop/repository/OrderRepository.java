82ea201e40ce2cedb711b2baf2954aff3ee5c6fa
김지웅 <godjw96@gmail.com>
2021-11-17 19:33:42 +0900

    Update OrderRepository.java

--- SpringJPA/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ SpringJPA/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -55,6 +55,16 @@ public class OrderRepository {
                 "select o from Order o" + " join fetch o.member m" + " join fetch o.delivery d", Order.class
         ).getResultList();
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                        "select distinct o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d" +
+                                " join fetch o.orderItems oi" +
+                                " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
 
 
