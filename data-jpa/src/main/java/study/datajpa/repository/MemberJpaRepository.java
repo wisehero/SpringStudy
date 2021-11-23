9685779292407033c681588bf96e5bee17e9b1a7
김지웅 <godjw96@gmail.com>
2021-11-24 00:33:00 +0900

    Create MemberJpaRepository.java

---
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,24 @@
+package study.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext // 이 어노테이션으로 하여금 엔티티매니저를 가져올 수 있음 (영속성 컨텍스트)
+    private EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+}
