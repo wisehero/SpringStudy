5ca86110b15903a3e678e7be9bd7e58a5e270100
김지웅 <godjw96@gmail.com>
2021-11-28 08:13:44 +0900

    Create MemberJpaRepository.java

---
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,22 @@
+package study.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+
+
+/**
+ *  순수 JPA 기반 레포지토리 만들기
+ *  스프링 데이터 JPA 공통 인터페이스 소개
+ *  스프링 데이터 JPA 공통 인터페이스 활용
+ *
+ *  기본 CRUD
+ *      저장
+ *      변경 -> 더티체킹
+ *      삭제
+ *      전체조회
+ *      단건조회
+ *      카운트
+ */
+@Repository
+public class MemberJpaRepository {
+    
+}
