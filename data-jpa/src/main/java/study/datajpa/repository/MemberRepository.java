140c3e2f1ddcf1ffd6c616441f56339acb145948
김지웅 <godjw96@gmail.com>
2021-11-28 13:42:20 +0900

    Update MemberRepository.java

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,7 +2,7 @@ package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
 
-@Repository
-public interface MemberRepository extends JpaRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
