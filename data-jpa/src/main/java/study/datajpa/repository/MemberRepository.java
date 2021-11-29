999f7f8a68860f49f45d8f018f4077efc80bfb84
김지웅 <godjw96@gmail.com>
2021-11-29 22:33:18 +0900

    Update MemberRepository.java

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,33 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    // 실행 시점에
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    // DTO로 직접 조회
+    @Query("select new study.datajpa.dto.MemberDto(m.Id, m.username, t.name)"
+            + "from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    // 파라미터 바인딩
+    @Query("select m from Member m where m.username = :name")
+    Member findMembers(@Param("name") String username);
+
+    // 컬렉션 파라미터 바인딩
+    @Query("select  m from Member m where m.username in : names")
+    List<Member> findByNames(@Param("names") List<String> names);
 }
