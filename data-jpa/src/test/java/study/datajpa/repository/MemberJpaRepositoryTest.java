7c9f451e646363b6d811df2844e79f982514eb1d
김지웅 <godjw96@gmail.com>
2021-12-03 06:43:04 +0900

    Update MemberJpaRepositoryTest.java

--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -46,7 +46,7 @@ public class MemberJpaRepositoryTest {
         assertThat(findMember2).isEqualTo(member2);
 
         // 리스트 조회 검증
-        List<Member> all  = memberJpaRepository.findAll();
+        List<Member> all = memberJpaRepository.findAll();
         assertThat(all.size()).isEqualTo(2);
 
         // 카운트 검증
@@ -57,7 +57,7 @@ public class MemberJpaRepositoryTest {
         memberJpaRepository.delete(member1);
         memberJpaRepository.delete(member2);
 
-        long deletedCount= memberJpaRepository.count();
+        long deletedCount = memberJpaRepository.count();
         assertThat(deletedCount).isEqualTo(0);
     }
 
@@ -73,5 +73,32 @@ public class MemberJpaRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("AAA");
         assertThat(result.get(0).getAge()).isEqualTo(20);
         assertThat(result.size()).isEqualTo(1);
+    }
+
+
+    @Test
+    public void paging() {
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        // when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        // 페이지 계산 공식 적용...
+        // totalPage = totalCount / size ....
+        // 마지막 페이지 ...
+        // 최초 페이지 ...
+
+        //then
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
     }
 }
