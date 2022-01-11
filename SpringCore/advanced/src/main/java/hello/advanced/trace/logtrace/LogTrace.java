c6a8aa59c9c0e9be4164a81a8e5404b0c60f31b3
김지웅 <godjw96@gmail.com>
2022-01-11 19:57:10 +0900

    수정

--- SpringCore/advanced/src/main/java/hello/advanced/trace/logtrace/LogTrace.java
+++ SpringCore/advanced/src/main/java/hello/advanced/trace/logtrace/LogTrace.java
@@ -4,6 +4,8 @@ import hello.advanced.trace.TraceStatus;
 
 public interface LogTrace {
     TraceStatus begin(String message);
+
     void end(TraceStatus status);
+
     void exception(TraceStatus status, Exception e);
 }
