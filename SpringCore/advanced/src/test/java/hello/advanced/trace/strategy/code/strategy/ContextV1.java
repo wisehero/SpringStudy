60a0a3c96fedc7a0bef69612a14626fdd462b16c
김지웅 <godjw96@gmail.com>
2022-01-14 06:02:37 +0900

    전략패턴

---
+++ SpringCore/advanced/src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV1.java
@@ -0,0 +1,22 @@
+package hello.advanced.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ContextV1 {
+
+    private Strategy strategy; // 변하는 알고리즘
+
+    public ContextV1(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    // 변하지 않는 실행 부분
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+        strategy.call(); // 위임
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+}
