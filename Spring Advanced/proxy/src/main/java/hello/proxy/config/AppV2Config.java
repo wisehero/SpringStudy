4f3d6833ef0c331b96f8850eb811f8b7608077ad
김지웅 <godjw96@gmail.com>
2022-01-29 01:48:22 +0900

    Update AppV2Config.java

--- Spring Advanced/proxy/src/main/java/hello/proxy/config/AppV2Config.java
+++ Spring Advanced/proxy/src/main/java/hello/proxy/config/AppV2Config.java
@@ -1,5 +1,8 @@
-package hello.proxy.app.v2;
+package hello.proxy.config;
 
+import hello.proxy.app.v2.OrderControllerV2;
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
