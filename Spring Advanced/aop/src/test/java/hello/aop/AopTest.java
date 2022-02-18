d148a283d116e8661c25c8aa9d61d22e62c77204
김지웅 <godjw96@gmail.com>
2022-02-18 18:08:24 +0900

    Create AopTest.java

---
+++ Spring Advanced/aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,44 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.*;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        System.out.println("isAopProxy, orderService=" + AopUtils.isAopProxy(orderService));
+        System.out.println("isAopProxy, orderRepository=" + AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
