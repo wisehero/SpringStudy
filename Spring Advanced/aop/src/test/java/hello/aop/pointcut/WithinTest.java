ed8f17d7eafcf83955c70b6976d4ce7321e2b983
김지웅 <godjw96@gmail.com>
2022-02-23 13:16:37 +0900

    Update WithinTest.java

--- Spring Advanced/aop/src/test/java/hello/aop/pointcut/WithinTest.java
+++ Spring Advanced/aop/src/test/java/hello/aop/pointcut/WithinTest.java
@@ -4,6 +4,7 @@ import hello.aop.member.MemberService;
 import hello.aop.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
@@ -25,13 +26,13 @@ public class WithinTest {
 
     @Test
     void withinExact() {
-        pointcut.setExpression("within(hello.aop.member.MemberServiceImpl");
-        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue()
+        pointcut.setExpression("within(hello.aop.member.MemberServiceImpl)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 
     @Test
     void withinStar() {
-        pointcut.setExpression("within(hello.aop.member.*Service*");
+        pointcut.setExpression("within(hello.aop.member.*Service*)");
         assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 
@@ -41,6 +42,17 @@ public class WithinTest {
         assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 
-    
+    @Test
+    @DisplayName("타겟의 타입에만 직접 적용, 인터페이스를 선정하면 안된다.")
+    void withSuperTypeFalse() {
+        pointcut.setExpression("within(hello.aop.member.MemberService)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
 
+    @Test
+    @DisplayName("execution은 타입 기반, 인터페이스를 선정 가능.")
+    void executionSuperTypeTrue() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
 }
