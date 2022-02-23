394913be944365c395423ffd6d8c3362970b9087
김지웅 <godjw96@gmail.com>
2022-02-23 13:09:58 +0900

    Create WithinTest.java

---
+++ Spring Advanced/aop/src/test/java/hello/aop/pointcut/WithinTest.java
@@ -0,0 +1,46 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class WithinTest {
+
+    // within  사용 시에는 부모 타입을 지정하면 안된다.
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void withinExact() {
+        pointcut.setExpression("within(hello.aop.member.MemberServiceImpl");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue()
+    }
+
+    @Test
+    void withinStar() {
+        pointcut.setExpression("within(hello.aop.member.*Service*");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void withinSubPackage() {
+        pointcut.setExpression("within(hello.aop..*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    
+
+}
