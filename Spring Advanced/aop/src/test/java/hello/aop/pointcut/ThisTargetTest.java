4163bcbbb31e93b846bc043b4cbf5876e2c14b06
김지웅 <godjw96@gmail.com>
2022-03-04 03:35:16 +0900

    Update ThisTargetTest.java

--- Spring Advanced/aop/src/test/java/hello/aop/pointcut/ThisTargetTest.java
+++ Spring Advanced/aop/src/test/java/hello/aop/pointcut/ThisTargetTest.java
@@ -46,7 +46,7 @@ public class ThisTargetTest {
         // this: 스프링 AOP 프록시 객체 대상
         // JDK 동적 프록시는 인터페이스를 기반으로 생성되므로 구현 클래스를 알 수 없음
         // CGLIB 프록시는 구현 클래스를 기반으로 생성되므로 구현 클래스를 알 수 있음
-        @Around("this(hello.aop.member.MemberServiceImpl")
+        @Around("this(hello.aop.member.MemberServiceImpl)")
         public Object doThis(ProceedingJoinPoint joinPoint) throws Throwable {
             log.info("[this-impl] {}", joinPoint.getSignature());
             return joinPoint.proceed();
