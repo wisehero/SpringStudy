54239b743b4b53d6c70d60b2718026ff2f8ed399
wisehero10 <godjw96@gmail.com>
2022-06-19 20:43:35 +0900

    chore : 불필요한 파일 삭제

--- Practice/boardDemo/src/main/java/board/boardDemo/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package board.boardDemo.repository;
-
-import board.boardDemo.domain.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
