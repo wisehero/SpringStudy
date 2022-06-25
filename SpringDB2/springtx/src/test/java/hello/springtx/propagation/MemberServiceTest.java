cb6e3803b787587d7dfbd1ccf4050bc06c70d43e
wisehero10 <godjw96@gmail.com>
2022-06-26 04:29:18 +0900

    트랜잭션 전파 활용1 - 예제 프로젝트 시작

---
+++ SpringDB2/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,32 @@
+package hello.springtx.propagation;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+	@Autowired
+	MemberService memberService;
+	@Autowired
+	MemberRepository memberRepository;
+	@Autowired
+	LogRepository logRepository;
+
+	@Test
+	void outerTxOff_success() {
+		// given
+		String username = "outerTxOff_success";
+		// when
+		memberService.joinV1(username);
+		// then
+		assertThat(memberRepository.find(username)).isNotNull();
+		assertThat(logRepository.find(username)).isNotNull();
+	}
+}
