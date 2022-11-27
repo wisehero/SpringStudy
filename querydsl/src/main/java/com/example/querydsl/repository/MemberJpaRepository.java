bfeb9738b26be1b973a1487f249ba80615dddecc
wisehero10 <godjw96@gmail.com>
2022-11-27 21:24:59 +0900

    # 동적 쿼리 Where

--- querydsl/src/main/java/com/example/querydsl/repository/MemberJpaRepository.java
+++ querydsl/src/main/java/com/example/querydsl/repository/MemberJpaRepository.java
@@ -16,6 +16,7 @@ import com.example.querydsl.dto.MemberTeamDto;
 import com.example.querydsl.dto.QMemberTeamDto;
 import com.example.querydsl.entity.Member;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Repository
@@ -88,5 +89,39 @@ public class MemberJpaRepository {
 				.leftJoin(member.team, team)
 				.where(builder)
 				.fetch();
+	}
+
+	public List<MemberTeamDto> search(MemberSearchCondition condition) {
+		return queryFactory
+				.select(new QMemberTeamDto(
+						member.id,
+						member.username,
+						member.age,
+						team.id,
+						team.name
+				))
+				.from(member)
+				.leftJoin(member.team, team)
+				.where(usernameEq(condition.getUsername()),
+						teamNameEq(condition.getTeamName()),
+						ageGoe(condition.getAgeGoe()),
+						ageLoe(condition.getAgeLoe()))
+				.fetch();
+	}
+
+	private BooleanExpression usernameEq(String username) {
+		return username.isEmpty() ? null : member.username.eq(username);
+	}
+
+	private BooleanExpression teamNameEq(String teamName) {
+		return teamName.isEmpty() ? null : team.name.eq(teamName);
+	}
+
+	private BooleanExpression ageGoe(Integer ageGoe) {
+		return ageGoe == null ? null : member.age.goe(ageGoe);
+	}
+
+	private BooleanExpression ageLoe(Integer ageLoe) {
+		return ageLoe == null ? null : member.age.loe(ageLoe);
 	}
 }
