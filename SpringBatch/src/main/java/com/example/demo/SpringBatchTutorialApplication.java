afd0287bd2efd502a02dbbbdec8bd64672f53c15
wisehero10 <godjw96@gmail.com>
2023-01-01 18:38:31 +0900

    # 스프링 배치 HelloWorld

--- SpringBatch/src/main/java/com/example/demo/SpringBatchTutorialApplication.java
+++ SpringBatch/src/main/java/com/example/demo/SpringBatchTutorialApplication.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing // batch 활성화
 @SpringBootApplication
 public class SpringBatchTutorialApplication {
 
