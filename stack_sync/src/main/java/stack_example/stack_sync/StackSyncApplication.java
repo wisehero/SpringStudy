09e75d6e00a6360ef084a4dcca06279f56716294
wisehero10 <godjw96@gmail.com>
2023-02-02 04:36:09 +0900

    재고 시스템으로 알아보는 동시성 이슈

---
+++ stack_sync/src/main/java/stack_example/stack_sync/StackSyncApplication.java
@@ -0,0 +1,13 @@
+package stack_example.stack_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackSyncApplication.class, args);
+	}
+
+}
---
+++ stack_sync/src/test/java/stack_example/stack_sync/StackSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package stack_example.stack_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
