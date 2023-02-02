213b4f18b6aae095229fbc3747534067a2f727df
wisehero10 <godjw96@gmail.com>
2023-02-03 01:24:28 +0900

    Syncrhonized 키워드 사용해보기

--- stack_sync/src/main/java/stack_example/stack_sync/domain/StockService.java
+++ stack_sync/src/main/java/stack_example/stack_sync/domain/StockService.java
@@ -12,8 +12,7 @@ public class StockService {
 		this.stockRepository = stockRepository;
 	}
 
-	@Transactional
-	public void decrease(Long id, Long quantity) {
+	public synchronized void decrease(Long id, Long quantity) {
 		// get stock
 		// 재고 감소
 		// 저장
